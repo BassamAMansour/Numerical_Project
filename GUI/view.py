580ef8b5bdb61361bd7f0a2e9d0603b5cda66141
Islam <Islamgamal_1997@hotmail.com>
2018-05-08 01:26:39 +0200

    GUI

--- GUI/view.py
+++ GUI/view.py
@@ -15,44 +15,52 @@ mode = 0
 instance = 0
 glob_i = 0
 glob_h = 0
+round_to = 5
 # Functions ---------------------------------------------------------------------------------
 def prepare_newton():
-    label_current_method.config(text = "       Method  :  Newton" , fg = 'GREEN')
+    label_current_method.config(text = "Method  :  Newton" , fg = 'GREEN')
+    additional_entry2.grid_forget()
     global method
     method = 1
 def prepare_fixed_point():
-    label_current_method.config(text = "       Method  :  F-Point" , fg = 'GREEN')
+    label_current_method.config(text = "Method  :  F-Point" , fg = 'GREEN')
+    additional_entry2.grid_forget()
     global method
     method = 2
 def prepare_bisection():
-    label_current_method.config(text = "       Method  :  Bisec." , fg = 'GREEN')
+    label_current_method.config(text = "Method  :  Bisec." , fg = 'GREEN')
+    additional_entry2.grid(row=11, column=2, sticky=W)
     global method
     method = 3
 def prepare_secant():
-    label_current_method.config(text = "       Method  :  Secant" , fg = 'GREEN')
+    label_current_method.config(text = "Method  :  Secant" , fg = 'GREEN')
+    additional_entry2.grid(row=11, column=2, sticky=W)
     global method
     method = 4
 def prepare_bierge_vieta():
-    label_current_method.config(text = "       Method  :  B-Vieta" , fg = 'GREEN')
+    label_current_method.config(text = "Method  :  B-Vieta" , fg = 'GREEN')
+    additional_entry2.grid_forget()
     global method
     method = 5
 def prepare_false_position():
-    label_current_method.config(text = "       Method  :  False-Pos" , fg = 'GREEN')
+    label_current_method.config(text = "Method  :  False-Pos" , fg = 'GREEN')
+    additional_entry2.grid(row=11, column=2, sticky=W)
     global method
     method = 6
 def is_slow():
-    label_current_mode.config(text = "       Mode     :  Slow" , fg = 'GREEN')
+    label_current_mode.config(text = "Mode     :  Slow" , fg = 'GREEN')
     global mode
     mode = 1
     iterate.grid(row = 0, rowspan = 15 ,columnspan = 4,sticky= W, column = 0)
 def is_fast():
-    label_current_mode.config(text = "       Mode     :  Fast" , fg = 'GREEN')
+    label_current_mode.config(text = "Mode     :  Fast" , fg = 'GREEN')
     global mode
     mode = 0
     iterate.grid_forget()
 def solve():
     function = functionEntry.get()
     initial = float(additional_entry.get())
+    initial2 = float(additional_entry2.get())
     solver = EquationSolver(function, 20, 0.00001)
     global method
     global mode
@@ -62,14 +70,15 @@ def solve():
     elif method == 2:
         instance = FixedPoint(function, initial)
     elif method == 3:
-        instance = Bisection()
+        instance = Bisection(function, initial, initial2)
     elif method == 4:
-        instance = Secant()
+        instance = Secant(function, initial, initial2)
     elif method == 5:
         instance = BiergeVieta(function, initial)
     elif method == 6:
-        instance = FalsePosition()
+        instance = FalsePosition(function, initial, initial2)
     height = len(instance.roots)
+
     if(mode == 0) :
         b = Label(frame, borderwidth=2, relief="solid", text="i", font=("Courier", 12), width=5, fg="RED",
                   bg="YELLOW")
@@ -79,6 +88,7 @@ def solve():
         b = Label(frame,borderwidth=2, relief="solid", text="Precision", font=("Courier", 12), width=12, fg="RED", bg="YELLOW")
         b.grid(sticky=W, row=0, column=2)
         width = 2
+        global round_to
         for i in range(0, height):  # Rows
             b = Label(frame, borderwidth=2, relief="groove", text=str(i + 1), font=("Courier", 12), width=5, fg="RED",
                       bg="YELLOW")
@@ -86,11 +96,15 @@ def solve():
             for j in range(0, width):  # Columns
                 if j == 0 :
                     root = instance.roots[i].root
+                    if root != None :
+                        root = round(root, round_to)
                     r = str(root)
                     b = Label(frame, borderwidth=2, relief="groove", text= r, font=("Courier", 12), width=12, fg="BLUE", bg="white")
                     b.grid(sticky=W, row=i + 1, column=j + 1)
                 else :
                     precision = instance.roots[i].precision
+                    if precision != None :
+                        p = round(precision, round_to)
                     p = str(precision)
                     b = Label(frame,borderwidth=2, relief="groove", text= p , font=("Courier", 12), width=12, fg="BLUE", bg="white")
                     b.grid(sticky=W, row=i + 1, column=j + 1)
@@ -119,11 +133,15 @@ def nextIteration():
         b.grid(sticky=W, row=glob_i + 1, column=0)
 
         root = instance.roots[glob_i].root
+        if root != None :
+            root = round(root, round_to)
         r = str(root)
         b = Label(frame, borderwidth=2, relief="groove", text=r, font=("Courier", 12), width=12, fg="BLUE", bg="white")
         b.grid(sticky=W, row=glob_i + 1, column=1)
 
         precision = instance.roots[glob_i].precision
+        if precision != None :
+          precision = round(precision, round_to)
         p = str(precision)
         b = Label(frame, borderwidth=2, relief="groove", text=p, font=("Courier", 12), width=12, fg="BLUE", bg="white")
         b.grid(sticky=W, row=glob_i + 1, column=2)
@@ -132,38 +150,44 @@ def nextIteration():
 root = Tk()
 root.title('Numerical Analysis')
 root.resizable(width=FALSE, height=FALSE)
-root.geometry('{}x{}'.format(1200, 800))
+root.geometry('{}x{}'.format(1250, 800))
 
 # Labels-----------------------------------------------------------------------------------------
 
-label_empty = Label(root, text = "Enter the function",font=("Courier", 20))
+label_empty = Label(root, text = "Enter the function",font=("Helvetica", 20))
 label_empty.grid(row = 0, column = 1)
 
-label_init = Label(root, text = "initial",font=("Courier", 15), fg = 'BLUE')
+label_init = Label(root, text = "initials",font=("Courier", 15), fg = 'BLUE')
 label_init.grid(row = 0, column = 2, sticky = W)
 
-label_current_method = Label(root, text = "       Method  :   None", font=("Helvetica", 20), fg = 'RED')
-label_current_method.grid(row = 13, column  = 1, sticky = W)
 
-label_current_mode = Label(root, text = "       Mode     :   None", font=("Helvetica", 20), fg = 'RED')
-label_current_mode.grid(row = 12, column  = 1, sticky = W)
+label_current_method = Label(root, text = "Method  :   None", font=("Helvetica", 17), fg = 'RED')
+label_current_method.grid(row = 7, column  = 3, sticky = W, rowspan = 5)
+
+label_current_mode = Label(root, text = "Mode     :   None", font=("Helvetica", 17), fg = 'RED')
+label_current_mode.grid(row = 11, column  = 3, sticky = W, rowspan = 5)
 
 label_enter_function = Label(root, text = "                        ", font=("Courier", 20))
 label_enter_function.grid(row = 10, column = 0)
 
 # Entries-----------------------------------------------------------------------------------------
 
-functionEntry = Entry(root, font=("Helvetica", 20))
+functionEntry = Entry(root, font=("Helvetica", 20), width = 23)
 functionEntry.grid(row = 10, column = 1)
 
 additional_entry = Entry(root, font=("Helvetica", 20), width = 5)
 additional_entry.grid(row = 10, column = 2,sticky = W)
 
+additional_entry2 = Entry(root, font=("Helvetica", 20), width = 5)
+additional_entry2.grid(row = 11, column = 2,sticky = W)
+additional_entry2.grid_forget()
+
+
 # buttons---------------------------------------------------------------------------------------
 
 
-evaluate = Button(root, text = "Solve", font=("Helvetica", 15), command = solve, width = 12)
-evaluate.grid(row = 14, column = 1)
+evaluate = Button(root, text = "Solve",borderwidth=4,relief="groove", font=("Helvetica", 15), command = solve, width = 28)
+evaluate.grid(row = 11, column = 1, sticky = W)
 
 labell = Label(root, text="                                                                                          ")
 labell.grid(row = 12, column = 2)
@@ -176,7 +200,7 @@ iterate.grid_forget()
 
 menu = Menu(root, font=("Courier", 20))
 root.config(menu = menu)
-submenu = Menu(menu, font=("Courier", 20))
+submenu = Menu(menu, font=("Courier", 20), borderwidth=2, relief="solid")
 menu.add_cascade(label = "Methods | ", menu = submenu)
 menu.add_separator()
 submenu.add_radiobutton(label = "Newton's" ,command = prepare_newton)
@@ -186,7 +210,7 @@ submenu.add_radiobutton(label = "Secant" ,command = prepare_secant)
 submenu.add_radiobutton(label = "Bierge Vieta", command = prepare_bierge_vieta)
 submenu.add_radiobutton(label = "False Position", command = prepare_false_position)
 
-submenu2 = Menu(menu, font=("Courier", 20))
+submenu2 = Menu(menu, font=("Courier", 20),borderwidth=2, relief="groove")
 menu.add_cascade(label = "Mode", menu = submenu2)
 submenu2.add_radiobutton(label = "Slow" ,command = is_slow)
 submenu2.add_radiobutton(label = "Fast" ,command = is_fast)
