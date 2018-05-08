289d269f5ad781689d588d3e32090746586393af
Islam <Islamgamal_1997@hotmail.com>
2018-05-08 02:13:52 +0200

    GUI Menus

--- GUI/view.py
+++ GUI/view.py
@@ -1,5 +1,8 @@
+from doctest import master
 from tkinter import *
 from tkinter.font import BOLD
+
+from six import b
 
 from equation_solvers.BiergeVieta import BiergeVieta
 from equation_solvers.Bisection import Bisection
@@ -10,12 +13,33 @@ from equation_solvers.NewtonRaphson import NewtonRaphson
 from equation_solvers.Root import Root
 from equation_solvers.Secant import Secant
 
+
+
+class popupWindow():
+    def __init__(self):
+        top=self.top=Toplevel(master)
+        top.resizable(width=FALSE, height=FALSE)
+        top.geometry('{}x{}'.format(200, 100))
+        self.l=Label(top, font=("Helvetica", 16), width = 20,text="Enter value")
+        self.l.pack()
+        self.e=Entry(top, font=("Helvetica", 16), width = 20)
+        self.e.pack()
+        self.b=Button(top, font=("Helvetica", 16),text='Ok',command=self.cleanup)
+        self.b.pack()
+    def cleanup(self):
+        global globalValue
+        globalValue = float(self.e.get())
+        self.value=self.e.get()
+        self.top.destroy()
+
+rounding = True
+globalValue = 0
 method = 0
 mode = 0
 instance = 0
 glob_i = 0
 glob_h = 0
-round_to = 5
+round_to = 6
 # Functions ---------------------------------------------------------------------------------
 def prepare_newton():
     label_current_method.config(text = "Method  :  Newton" , fg = 'GREEN')
@@ -52,6 +76,13 @@ def is_slow():
     global mode
     mode = 1
     iterate.grid(row = 0, rowspan = 15 ,columnspan = 4,sticky= W, column = 0)
+def is_chopping():
+    global rounding
+    rounding = False
+
+def is_rounding():
+    global rounding
+    rounding = True
 def is_fast():
     label_current_mode.config(text = "Mode     :  Fast" , fg = 'GREEN')
     global mode
@@ -83,9 +114,9 @@ def solve():
         b = Label(frame, borderwidth=2, relief="solid", text="i", font=("Courier", 12), width=5, fg="RED",
                   bg="YELLOW")
         b.grid(sticky=W, row=0, column=0)
-        b = Label(frame,borderwidth=2, relief="solid", text="Root", font=("Courier", 12), width=12, fg="RED", bg="YELLOW")
+        b = Label(frame,borderwidth=2, relief="solid", text="Root", font=("Courier", 12), width=15, fg="RED", bg="YELLOW")
         b.grid(sticky=W, row=0, column=1)
-        b = Label(frame,borderwidth=2, relief="solid", text="Precision", font=("Courier", 12), width=12, fg="RED", bg="YELLOW")
+        b = Label(frame,borderwidth=2, relief="solid", text="Precision", font=("Courier", 12), width=15, fg="RED", bg="YELLOW")
         b.grid(sticky=W, row=0, column=2)
         width = 2
         global round_to
@@ -99,32 +130,36 @@ def solve():
                     if root != None :
                         root = round(root, round_to)
                     r = str(root)
-                    b = Label(frame, borderwidth=2, relief="groove", text= r, font=("Courier", 12), width=12, fg="BLUE", bg="white")
+                    b = Label(frame, borderwidth=2, relief="groove", text= r, font=("Courier", 12), width=15, fg="BLUE", bg="white")
                     b.grid(sticky=W, row=i + 1, column=j + 1)
                 else :
                     precision = instance.roots[i].precision
                     if precision != None :
-                        p = round(precision, round_to)
+                        precision = round(precision, round_to)
                     p = str(precision)
-                    b = Label(frame,borderwidth=2, relief="groove", text= p , font=("Courier", 12), width=12, fg="BLUE", bg="white")
+                    b = Label(frame,borderwidth=2, relief="groove", text= p , font=("Courier", 12), width=15, fg="BLUE", bg="white")
                     b.grid(sticky=W, row=i + 1, column=j + 1)
     else :
         b = Label(frame, borderwidth=2, relief="solid", text="i", font=("Courier", 12), width=5, fg="RED",
                   bg="YELLOW")
         b.grid(sticky=W, row=0, column=0)
-        b = Label(frame, borderwidth=2, relief="solid", text="Root", font=("Courier", 12), width=12, fg="RED",
+        b = Label(frame, borderwidth=2, relief="solid", text="Root", font=("Courier", 12), width=15, fg="RED",
                   bg="YELLOW")
         b.grid(sticky=W, row=0, column=1)
-        b = Label(frame, borderwidth=2, relief="solid", text="Precision", font=("Courier", 12), width=12, fg="RED",
+        b = Label(frame, borderwidth=2, relief="solid", text="Precision", font=("Courier", 12), width=15, fg="RED",
                   bg="YELLOW")
         b.grid(sticky=W, row=0, column=2)
         global glob_i, glob_h
         glob_i = 0
         glob_h = height
         nextIteration()
+def popup():
+    w = popupWindow()
+
 def nextIteration():
     global glob_i
     global glob_h
+    global rounding
     if glob_i >=glob_h :
         return
     else :
@@ -134,16 +169,16 @@ def nextIteration():
 
         root = instance.roots[glob_i].root
         if root != None :
-            root = round(root, round_to)
+                root = round(root, round_to)
         r = str(root)
-        b = Label(frame, borderwidth=2, relief="groove", text=r, font=("Courier", 12), width=12, fg="BLUE", bg="white")
+        b = Label(frame, borderwidth=2, relief="groove", text=r, font=("Courier", 12), width=15, fg="BLUE", bg="white")
         b.grid(sticky=W, row=glob_i + 1, column=1)
 
         precision = instance.roots[glob_i].precision
         if precision != None :
           precision = round(precision, round_to)
         p = str(precision)
-        b = Label(frame, borderwidth=2, relief="groove", text=p, font=("Courier", 12), width=12, fg="BLUE", bg="white")
+        b = Label(frame, borderwidth=2, relief="groove", text=p, font=("Courier", 12), width=15, fg="BLUE", bg="white")
         b.grid(sticky=W, row=glob_i + 1, column=2)
         glob_i += 1
 
@@ -211,10 +246,25 @@ submenu.add_radiobutton(label = "Bierge Vieta", command = prepare_bierge_vieta)
 submenu.add_radiobutton(label = "False Position", command = prepare_false_position)
 
 submenu2 = Menu(menu, font=("Courier", 20),borderwidth=2, relief="groove")
-menu.add_cascade(label = "Mode", menu = submenu2)
+menu.add_cascade(label = "Mode | ", menu = submenu2)
 submenu2.add_radiobutton(label = "Slow" ,command = is_slow)
 submenu2.add_radiobutton(label = "Fast" ,command = is_fast)
+
+
+submenu3 = Menu(menu, font=("Courier", 20),borderwidth=2, relief="groove")
+menu.add_cascade(label = "Approximation | ", menu = submenu3)
+submenu3.add_radiobutton(label = "Rounding" ,command = is_rounding)
+submenu3.add_radiobutton(label = "Chopping" ,command = is_chopping)
+
+submenu4 = Menu(menu, font=("Courier", 20),borderwidth=2, relief="groove")
+menu.add_cascade(label = "Constants", menu = submenu4)
+submenu4.add_radiobutton(label = "Precision" ,command = popup)
+submenu4.add_radiobutton(label = "Maximum Iteration" ,command = popup)
+submenu4.add_radiobutton(label = "Segnificant Figures" ,command = popup)
+
 
 frame = Frame(root)
 frame.grid(sticky = W, row = 20, columnspan = 6)
 root.mainloop()
+
+
