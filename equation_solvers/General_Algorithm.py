ffd4f431a0050dc419fb4c7e8c04045194c5825b
youmna_dwidar <youmnadwidar@hotmail.com>
2018-05-11 17:42:53 +0200

    Merge branch 'master' of https://github.com/Velnster/Numerical_Project
    # Conflicts:
    #	.idea/Numerical Project.iml
    #	.idea/misc.xml

--- equation_solvers/General_Algorithm.py
+++ equation_solvers/General_Algorithm.py
@@ -3,16 +3,18 @@ from symtable import Symbol
 import numpy
 from sympy import Poly, simplify
 
-from equation_solvers import FalsePosition
+from equation_solvers import FalsePosition, Bisection
 from equation_solvers.EquationSolver import EquationSolver
 from equation_solvers.Root import Root
 from equation_solvers.Secant import Secant
 
 
 class General_Algorithm(EquationSolver):
-    CONSTANT = 5
+    CONSTANT = 3
     equation = ""
     length = 0
+    initial_1 = None
+    initial_2 = None
 
     def __init__(self, equation, max_iterations=EquationSolver.DEFAULT_MAX_ITERATIONS,
                  precision=EquationSolver.DEFAULT_EPSILON):
@@ -22,31 +24,40 @@ class General_Algorithm(EquationSolver):
         self.equation = equation
 
     def solve_polynomial(self, initial_1, initial_2):
+        self.initial_1 = initial_1
+        self.initial_2 = initial_2
+        difference = abs(self.initial_2 - self.initial_1)
+        while difference < 1:
+            difference *= 10
+        interval = numpy.linspace(self.initial_1, self.initial_2, difference * self.CONSTANT, True)
+        for i in range(0, len(interval) - 1):
+            bisection = Bisection(self.equation, interval[i], interval[i + 1])
+            root = bisection.get_root()
+            if root is not None:
+                if bisection.root_found :
+                    root = bisection.roots[-1]
+                if not self.exist(root):
+                    self.add_root(root)
 
-        false_position = FalsePosition(self.equation, initial_1, initial_2, self.max_iterations,
-                                       self.precision)
-        last_root = false_position.getRoot()
-        if last_root is None:
-            secant = Secant(self.equation, initial_1, initial_2, self.max_iterations, self.precision)
-            secant.start_root_finding()
-            last_root = secant.roots[-1]
-        if not self.exist(last_root):
-            self.add_root(last_root)
-        if abs(initial_2 - initial_1) > EquationSolver.DEFAULT_EPSILON:
-            self.solve_polynomial(initial_1, last_root.root - EquationSolver.DEFAULT_EPSILON)
-            self.solve_polynomial(last_root.root + EquationSolver.DEFAULT_EPSILON, initial_2)
 
-    # interval = numpy.linspace(self.initial_1, self.initial_2, abs(self.initial_2 - self.initial_1) * self.CONSTANT,
-    #                           True)
-    # for i in range(len(interval) - 1):
-    #     false_position = FalsePosition(equation, interval[i], interval[i + 1])
-    #     if false_position.getRoot() is not None:
-    #         self.roots.extend(false_position.roots)
-    #     elif len(self.roots) < length:
-    #         secant.start_root_finding()
-    #         self.roots.extend(secant.roots)roots
+
     def exist(self, root: Root):
         for i in range(len(self.roots)):
-            if abs(root.root - self.roots[i].root) < EquationSolver.DEFAULT_EPSILON:
+            if abs(root.root - self.roots[i].root) < EquationSolver.DEFAULT_EPSILON :
                 return True
         return False
+
+# false_position = FalsePosition(self.equation, initial_1, initial_2, self.max_iterations,
+#                                self.precision)
+# last_root = false_position.getRoot()
+# if last_root is None:
+#     secant = Secant(self.equation, initial_1, initial_2, self.max_iterations, self.precision)
+#     secant.start_root_finding()
+#     last_root = secant.roots[-1]
+# else:
+#     last_root = false_position.roots[-1]
+# if not self.exist(last_root):
+#     self.add_root(last_root)
+# if abs(initial_2 - initial_1) > .1:
+#     self.solve_polynomial(initial_1, last_root.root - EquationSolver.DEFAULT_EPSILON)
+#     self.solve_polynomial(last_root.root + EquationSolver.DEFAULT_EPSILON, initial_2)
--- equation_solvers/__init__.py
+++ equation_solvers/__init__.py
@@ -6,9 +6,9 @@ from equation_solvers.FixedPoint import FixedPoint
 from equation_solvers.General_Algorithm import General_Algorithm
 from equation_solvers.NewtonRaphson import NewtonRaphson
 
-# z = FixedPoint("2*x*(1-x) - x",0.1)
-equation = "x**2 - 1"
+z = FixedPoint("2*x*(1-x) - x",0.1)
+equation = "x**3 -4*x"
 general = General_Algorithm(equation)
-general.solve_polynomial( 0 , 2)
+general.solve_polynomial(-1, 3)
+print(general.roots)
 print("")
-
